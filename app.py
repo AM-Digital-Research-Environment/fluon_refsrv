fecb6d4015eb5355c5ba41b8e0e7956cb0f43c4e
Oliver Baumann <oliver.baumann@uni-bayreuth.de>
2022-11-21 18:36:40 +0100

    Document responses and payloads

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restx import Resource, Api, fields
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -7,50 +7,98 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
         )
+
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
+app.config['RESTX_MASK_SWAGGER'] = False
+app.config['RESTX_MASK_HEADER'] = None
+
 api = Api(
         app,
         version='1.0',
-        title='RefSrv API',
-        description='Reference implementation of a server for extending fluid ontologies'
+        title='Fluid Ontologies API',
+        description='Reference implementation of a server handling fluid ontologies.'
       )
 
-ns = api.namespace('query', description='query stuff')
+
+queries = api.namespace(
+        'Queries',
+        path="/api/v1/queries",
+        description='Endpoints for manipulating raw search queries'
+        )
 
 queryObject = api.model('Query', {
-    'query': fields.String(required=True, description='The extended query')
-    })
-rankingObject = api.model('Ranking', {
-    'ids': fields.List(fields.String, description='The list of IDs returned from the search'),
-    'meta': fields.String
+    'query': fields.String(
+        required=True,
+        description='The original query, extended by additional keywords',
+        example="foo bar baz"
+        )
     })
 
 
-@ns.route('/api/v1/keywords/<int:user>')
-@ns.param('user', 'The user-ID')
-@ns.param('q', 'The search query', _in='query')
+@queries.route('/extend/<int:user>')
+@queries.param('user', 'The user-ID')
+@queries.param('q', 'The search query', _in='query')
 class Keywords(Resource):
-    @ns.doc('get_keywords')
-    @ns.marshal_with(queryObject)
+    @queries.response(400, "Missing query parameter")
+    @queries.marshal_with(queryObject)
     def get(self, user):
         args = request.args.to_dict()
-        print('logging user', user)
+        if 'q' not in args:
+            abort(400, "Missing query parameter 'q'")
         return {'query': args['q'] + ' ' + ' '.join(['foo', 'bar'])}
 
 
-@ns.route('/api/v1/rank/<int:user>')
-@ns.param('user', 'The user-ID')
-@ns.param('ids', 'The IDs to rank and filter', _in='body')
+ranking = api.namespace(
+        'Ranking',
+        path="/api/v1/rank",
+        description='Endpoints for ranking search results'
+        )
+
+rankingPayload = api.model('Ranking payload', {
+    'ids': fields.List(
+        fields.String,
+        required=True,
+        description="A list of entity-IDs",
+        example='["foo:345"]'
+        )
+    })
+
+rankingObject = api.model('Ranking', {
+    'ids': fields.List(
+        fields.String,
+        required=True,
+        description='A potentially reordered and filtered list of IDs returned from the search',
+        example='["foo:345", "bar:678"]',
+        strict=False,
+        validate=False
+        ),
+    'meta': fields.String(
+        required=True,
+        description="Metadata describing the re-ranking; a JSON array",
+        example='[{"reason": "profile"}, {"removed": ["bar:678"]}]',
+        strict=False,
+        validate=False
+        )
+    })
+
+
+@ranking.route('/<int:user>')
+@ranking.param('user', 'The user-ID')
 class Ranking(Resource):
-    @ns.doc('post_ids')
-    @ns.marshal_with(rankingObject)
+    @ranking.expect(rankingPayload)
+    @ranking.marshal_with(rankingObject)
+    @ranking.response(400, "Missing field 'ids' in request payload")
     def post(self, user):
-        args = request.get_json()
-        print('logging user', user)
-        args = sorted([int(x) for x in args])
-        meta = json.dumps({'some': 'thing', 'another': [1, 2, 3]})
-        return {'ids': args, 'meta': meta}
+        payload = request.get_json()
+        if 'ids' not in payload:
+            abort(400, "Missing field 'ids' in request payload")
+
+        ids = payload['ids']
+
+        response = sorted(ids, key=lambda x: x.split(':')[1])
+        meta = json.dumps([{'reason': 'profile'}, {'removed': 'bar:678'}])
+        return {'ids': response, 'meta': meta}
 
 
 if __name__ == '__main__':
-    print("ARGH")
     app.run(debug=True, host="0.0.0.0")
